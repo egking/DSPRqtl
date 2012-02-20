e0b560f1ce3c732397d90f2e6d7b193e67ae9fc9
Elizabeth King <egking@uci.edu>
2012-02-19 22:16:24 -0800

    add DSPRqtl-package.R and update description

---
+++ R/DSPRqtl-package.R
@@ -0,0 +1,26 @@
+##' DSPRqtl
+##' 
+##' Analysis of DSPR phenotypes
+##' 
+##' \tabular{ll}{
+##'   Package: \tab DSPRqtl\cr
+##'   Type: \tab Package\cr
+##'   Version: \tab 1.0-1\cr
+##'   Date: \tab 2012-02-19\cr
+##'   License: \tab GPL-2\cr
+##'   LazyLoad: \tab yes\cr
+##'   LazyData: \tab yes\cr
+##' }
+##' 
+##' @name DSPRqtl-package
+##' 
+##' @aliases DSPRqtl-package DSPRqtl
+##' 
+##' @docType package
+##' 
+##' @author Elizabeth King
+##' 
+##' @keywords package
+NULL
+
+
