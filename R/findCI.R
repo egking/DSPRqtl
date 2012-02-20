419aa8a8bd0e0c4f616e7d4268c2453b0c1bd507
Elizabeth King <egking@uci.edu>
2012-02-19 21:46:24 -0800

    document findCI

--- R/findCI.R
+++ R/findCI.R
@@ -1,18 +1,38 @@
-##' .. content for \description{} (no empty lines) ..
+##' Calculate LOD support interval
 ##'
-##' .. content for \details{} ..
-##' @title 
-##' @param peakChr 
-##' @param peakPos 
-##' @param qtldat 
-##' @param peak 
-##' @param drop 
-##' @return 
-##' @author Elizabeth King
-findCI<-function(peakChr,peakPos,qtldat,peak,drop)
+##' \code{findCI} calculates the LOD support interval for a given LOD peak.
+##' 
+##' @title Calculate LOD support interval
+##' 
+##' @param peakChr character vector of length one. 
+##' Must be one of the major chromosome arms in the 
+##' \emph{Drosophila} genome ('X','2L','2R','3L',or '3R').
+##' 
+##' @param peakPos numeric vector of length one. A position in 
+##' base pairs in the DSPR position list (every 10kb). See 
+##' \code{\link{positionlist_wgenetic}}.
+##'
+##' @param qtldat \code{data.frame} of chromosome, position, 
+##' and LOD scores (column names chr,Ppos,Gpos,LOD). List element LODscores 
+##' from \code{\link{DSPRscan}}.
+##' 
+##' @param peak numeric vector of length one consisting of the peak LOD score.
+##'  
+##' @param LODdrop numeric vector of length one consisting of the LOD drop to be used. 
+##' Default value is 2 which approximates a 95% confidence interval.
+##' 
+##' @return A \code{data.frame} with two rows containing the chromosome, physical position (bp), 
+##' genetic position (cM) and LOD scores corresponding to the lower and upper bound. 
+##' 
+##' @author Elizabeth King (\email{egking@@uci.edu})
+##' 
+##' @export
+##' 
+##' 
+findCI<-function(peakChr,peakPos,qtldat,peak,LODdrop)
 {
   startIndex<-which(qtldat$chr==peakChr & qtldat$Ppos==peakPos)
-  l.ci<-peak-drop  #drop threshold
+  l.ci<-peak-LODdrop  #drop threshold
   start1<-startIndex #this one is for the other side of the peak
   #one side
   while(qtldat$LOD[start1]>l.ci & (start1-1) >= 1) 
@@ -27,5 +47,5 @@ findCI<-function(peakChr,peakPos,qtldat,peak,drop)
   }
   CIs<-qtldat[c(start1,startIndex),]
   rownames(CIs)<-c('Lower Bound','Upper Bound')
-  return()
+  return(CIs)
 }
