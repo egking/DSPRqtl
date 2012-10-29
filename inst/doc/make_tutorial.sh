5cafe4db7553839c5fd04c79a7c7ec23a90b13b8
Kevin Middleton <kmm@csusb.edu>
2012-10-29 15:01:56 -0500

    Have make_tutorial.sh clean up after it's done.

--- inst/doc/make_tutorial.sh
+++ inst/doc/make_tutorial.sh
@@ -3,3 +3,4 @@
 RMDFILE=DSPRqtl-intro
 Rscript -e "require(knitr); require(markdown); knit('DSPRqtl-intro.Rmd', 'DSPRqtl-intro.md')"
 pandoc -N --include-in-header=header.tex -s --from=markdown DSPRqtl-intro.md -o DSPRqtl-intro.pdf
+rm DSPRqtl-intro.md
