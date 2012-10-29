530312772d8d19c213bf884700a0f3e8216c34ee
Kevin Middleton <kmm@csusb.edu>
2012-10-29 14:29:46 -0500

    Convert DSPRqtl-intro to .Rmd and associated files.

---
+++ inst/doc/make_tutorial.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+RMDFILE=DSPRqtl-intro
+Rscript -e "require(knitr); require(markdown); knit('DSPRqtl-intro.Rmd', 'DSPRqtl-intro.md')"
+pandoc -N --include-in-header=header.tex -s --from=markdown DSPRqtl-intro.md -o DSPRqtl-intro.pdf
