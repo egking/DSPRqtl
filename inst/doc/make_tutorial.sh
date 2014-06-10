69b2a8cb5560b9473cd790a14c35547f2a5dc008
Kevin Middleton <middletonk@missouri.edu>
2014-06-09 19:06:31 -0500

    Update vignette to use knitr for VignetteBuilder and Rmd. Remove old build scripts.

--- inst/doc/make_tutorial.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-Rscript -e "require(knitr); require(markdown); knit('DSPRqtl-intro.Rmd', 'DSPRqtl-intro.md')"
-pandoc -N --include-in-header=header.tex -s --from=markdown DSPRqtl-intro.md -o DSPRqtl-intro.pdf
